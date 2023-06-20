f9e96d801f756ca0aafa98601cfe657ecf030d39
DmitryStarostin <Sad12321@mail.ru>
2023-06-20 22:41:17 +0400

    Example004 extended

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,5 @@
-﻿double NumberA = 12;
-int NumberB = 5;
-Console.WriteLine(NumberA / NumberB);
+﻿double NumberA = new Random().Next(1,10);
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1,10);
+Console.WriteLine(NumberB);
+Console.WriteLine(NumberA + NumberB);
