872063b519f141dfed8a3b2a279f521f7b4255bd
DmitryStarostin <Sad12321@mail.ru>
2023-06-20 22:51:14 +0400

    Example005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter your name ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha"){
+    Console.WriteLine("Wow, it's Masha!");
+}
+else {
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
